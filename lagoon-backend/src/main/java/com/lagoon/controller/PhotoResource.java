55074ed000b17d6c918f1ef14a09937466629e10
Mc Kinley Tolentino <tolentino.mckinley@gmail.com>
2019-06-11 20:52:16 +0800

    Final

--- lagoon-backend/src/main/java/com/lagoon/controller/PhotoResource.java
+++ lagoon-backend/src/main/java/com/lagoon/controller/PhotoResource.java
@@ -1,7 +1,6 @@
 package com.lagoon.controller;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,17 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -36,9 +30,6 @@ public class PhotoResource {
 
 	@Autowired
 	private PhotoDao photoService;
-	
-	@Autowired
-	RestTemplate restTemplate; //temp
 
 	@RequestMapping(value = "/photo/upload", method = RequestMethod.POST)
 	public String upload(HttpServletResponse response, HttpServletRequest request) {
@@ -80,14 +71,6 @@ public class PhotoResource {
 	public ResponseEntity<Photo> getPhotoByPhotoId(@RequestBody Long photoId) {
 		Photo photo = photoService.findByPhotoId(photoId);
 		photo.setImageName(LagoonUtility.imageToByte(photo.getImageName()));
-		
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<User> entity = new HttpEntity<User>(headers);
-		
-		User photoaa =  restTemplate.exchange("http://localhost:8080/user/getAllUsers",HttpMethod.GET,entity, User.class).getBody();
-		System.out.println(">>>>>>>>>>> "+photoaa.getFirstName());
 		return new ResponseEntity<Photo>(photo, HttpStatus.OK);
 	}
 
